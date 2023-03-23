5de52417c40464dd36e572048fdb0d375a90585a
Cookiekira <1308132372@qq.com>
2023-03-23 21:55:47 +0800

    chore: Giscus separate line

--- components/Giscus.tsx
+++ components/Giscus.tsx
@@ -6,20 +6,30 @@ export function Comment() {
   const { isDarkMode } = useDarkMode()
   const theme = isDarkMode ? 'dark_dimmed' : 'light'
   return (
-    <Giscus
-      id='comments'
-      repo='Cookiekira/notion-blog'
-      repoId='R_kgDOJNNpnA'
-      category='Announcements'
-      categoryId='DIC_kwDOJNNpnM4CVFFt'
-      mapping='pathname'
-      term='Welcome to leave your comment!'
-      reactionsEnabled='1'
-      emitMetadata='0'
-      inputPosition='top'
-      theme={theme}
-      lang='zh-CN'
-      loading='lazy'
-    />
+    <>
+      <hr
+        style={{
+          margin: '50px 0 50px 0',
+          border: 0,
+          borderTop: '1px solid var(--fg-color-1)',
+          width: '100%'
+        }}
+      />
+      <Giscus
+        id='comments'
+        repo='Cookiekira/notion-blog'
+        repoId='R_kgDOJNNpnA'
+        category='Announcements'
+        categoryId='DIC_kwDOJNNpnM4CVFFt'
+        mapping='title'
+        term='Welcome to leave your comment!'
+        reactionsEnabled='1'
+        emitMetadata='0'
+        inputPosition='top'
+        theme={theme}
+        lang='zh-CN'
+        loading='lazy'
+      />
+    </>
   )
 }
